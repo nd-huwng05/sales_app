24f541514c4de5a8cea57dd30bbd47fca3b9364a
nd-huwng05 <nguyendinhhung290605@gmail.com>
2025-11-05 00:25:37 +0700

    update

--- app/index.py
+++ app/index.py
@@ -8,14 +8,14 @@ def index():
     q = request.args.get("q")
     cates = dao.load_categories()
     products = dao.load_product(q = q)
-    return render_template("index.html", cates = cates, products=products)
+    return render_template("index.html", products=products)
 
 @app.route("/product")
 def get_data():
     category_id = request.args.get("cates_id")
     cates = dao.load_categories()
     products = dao.load_product(category_id = int(category_id))
-    return render_template("index.html", cates=cates, products=products)
+    return render_template("index.html", products=products)
 
 @app.route("/products/<int:id>")
 def details(id):
@@ -28,6 +28,7 @@ def common_attribute():
     return {
         "cates": dao.load_categories()
     }
+
 if __name__ == "__main__":
     with app.app_context():
         app.run(debug=True)
